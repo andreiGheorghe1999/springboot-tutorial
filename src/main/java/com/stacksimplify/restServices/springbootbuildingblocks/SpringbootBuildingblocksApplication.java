a887a0c72cf958dead02e44c8dc3e75e28a736b4
andreiGheorghe1999 <61749667+andreiGheorghe1999@users.noreply.github.com>
2022-06-22 21:12:39 +0300

    Add comment to branch

--- src/main/java/com/stacksimplify/restServices/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restServices/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
@@ -2,7 +2,7 @@ package com.stacksimplify.restServices.springbootbuildingblocks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Main application class
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
