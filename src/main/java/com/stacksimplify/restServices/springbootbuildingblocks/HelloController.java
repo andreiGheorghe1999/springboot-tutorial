6fc003d70bfeca001f50308a63c226d936fa42a8
andreiGheorghe1999 <61749667+andreiGheorghe1999@users.noreply.github.com>
2022-07-12 19:41:34 +0300

    Internationalization - language related features

--- src/main/java/com/stacksimplify/restServices/springbootbuildingblocks/HelloController.java
+++ src/main/java/com/stacksimplify/restServices/springbootbuildingblocks/HelloController.java
@@ -10,8 +10,14 @@
  */
 package com.stacksimplify.restServices.springbootbuildingblocks;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 /**
  * Class name: Hello.java Copyright (c)2022, GMV All rights reserved.
@@ -22,7 +28,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController
 {
-
+  @Autowired
+  private ResourceBundleMessageSource resourceBundleMessageSource;
   @GetMapping("/helloWorld")
   public String helloWorld()
   {
@@ -33,5 +40,12 @@ public class HelloController
   public UserDetails getUserObject()
   {
     return new UserDetails("Andrei", "Gheorghe", "Bucuresti");
+  }
+
+  @GetMapping("/hello-int")
+  public String getMessagesInI18Nformat(@RequestHeader(value = "Accept-Language", required = false) String locale){
+    //return resourceBundleMessageSource.getMessage("label.hello", null,new Locale(locale));
+    //Better : the localeContextHolder does the job for us of identyfying the system language
+    return resourceBundleMessageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
   }
 }
--- src/main/java/com/stacksimplify/restServices/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restServices/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
@@ -1,18 +1,29 @@
 package com.stacksimplify.restServices.springbootbuildingblocks;
 
-import org.apache.catalina.servlets.WebdavServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
-import javax.servlet.annotation.WebServlet;
+import java.util.Locale;
 
 // Main application class
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
+    }
+
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver headerLocaleResolver = new AcceptHeaderLocaleResolver();
+        headerLocaleResolver.setDefaultLocale(Locale.US);
+        return headerLocaleResolver;
+    }
+    public ResourceBundleMessageSource messageSource(){
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
 }
